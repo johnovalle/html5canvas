d8c68992a9e58268783a5cb39831106490fea0fc
kugyousha <johnovalle@gmail.com>
2015-02-25 22:55:38 +0000

    fixed 'ceelo' and 'tracy' rolls

--- dice/ceelo.js
+++ dice/ceelo.js
@@ -4,11 +4,13 @@ function init(num) {
   var turnOver = false;
   while (!playerScore && !turnOver) {
       var theRoll = roll();
+      console.log(theRoll);
       playerScore = score(theRoll); 
   }
   console.log(theRoll);
   console.log(playerScore);
 }
+
 function die() {
   return (Math.floor(Math.random()*6) + 1);
 }
@@ -29,13 +31,12 @@ function score(roll) {
 }
 
 function evaluate(roll){
-    var duplicates = [];
-    var value = 0;
-    if (roll === [4,5,6]){
+    console.log(roll);
+    if (roll[0] === 4 && roll[1] === 5 && roll[2] === 6){
       turnOver = true;
       return 100;
     }
-    else if (roll === [1,2,3]) {
+    else if (roll[0] === 1 && roll[1] === 2 && roll[2] === 3) {
       turnOver = true;
       return -10;
     }
@@ -55,4 +56,7 @@ function evaluate(roll){
         return false;
     }
 }
+// x = roll();
+// console.log(x);
+// console.log(evaluate(x));
 init(2);
