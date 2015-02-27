b82588e1944575baabee7b4cbfe6c245489eb89c
kugyousha <johnovalle@gmail.com>
2015-02-27 16:57:23 -0500

    add fillMap function to map.js

--- mau/map.js
+++ mau/map.js
@@ -1,24 +1,35 @@
 function generateMap(height,width){
     var map = [];
-    for(var i = 0; i<height; i++){
+    for(var row = 0; row<height; row++){
         map.push([]);
-        for(var cols = 0; cols<width; cols++){
-            var tile = getTerrain();
-            map[i].push(tile);
+        for(var col = 0; col<width; col++){
+            blank_sectors.push([row,col]);
+            map[row].push("BLANK");
         }
     }
     return map;
 }
+
+var blank_sectors = [];
 var terrain = ['O', 'M', 'S', 'R', 'D', 'L'];
+
 function getTerrain(){
     var terrainType = Math.floor(Math.random()*terrain.length);
-    console.log(terrainType);
     return terrain[terrainType];
 }
 
-var map = generateMap(10,10);
-console.log(map);
+function fillMap(){
+    while (blank_sectors.length > 0){
+       var random_index = Math.floor(Math.random()*blank_sectors.length);
+       var selected_sector = blank_sectors[random_index];
+       blank_sectors.splice(random_index,1);
+       map[selected_sector[0]][selected_sector[1]] = getTerrain();
+    }
+}
 
+var map = generateMap(10,10);
+fillMap();
+console.log(map);
 //generate blank map
 //get array
 //while array contains blanks fill in and pop coordinates
