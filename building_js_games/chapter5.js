5b09b644f4b9807de16cff13b49da0f279c2d838
kugyousha <johnovalle@gmail.com>
2015-03-04 23:54:26 -0500

    rotate cannon -not working

--- building_js_games/chapter5.js
+++ building_js_games/chapter5.js
@@ -3,46 +3,49 @@
 var Game = {
   canvas: undefined,
   canvasContext: undefined,
-  //rectanglePosition: 0,
   backgroundSprite: undefined,
-  ballonSprite: undefined,
-  balloonPosition: { x: 0, y: 50 },
-  balloonPosition2: { x: 0, y: 250 },
-  balloonPosition3: { x: 0, y: 450 },
-  backgroundMusic: undefined
+  cannonBarrelSprite: undefined,
+  mousePosition: { x: 0, y: 0 },
+  cannonPosition: { x: 72, y: 405 },
+  cannonOrigin: { x: 34, y: 34 },
+  cannonRotation: 0  
 };
 
 Game.start = function () {
   Game.canvas = document.getElementById("myCanvas");
   Game.canvasContext = Game.canvas.getContext("2d");
-  Game.ballonSprite = new Image();
-  Game.ballonSprite.src = "spr_balloon.png";
   Game.backgroundSprite = new Image();
   Game.backgroundSprite.src = "spr_background.jpg";
-  Game.backgroundMusic = new Audio();
-  Game.backgroundMusic.src = "snd_music.mp3";
-  Game.backgroundMusic.play();
-  Game.backgroundMusic.volume = 0.4;
+  Game.cannonBarrelSprite = new Image();
+  Game.cannonBarrelSprite.src = "spr_cannon_barrel.png";
   window.setTimeout(Game.mainLoop, 500);
 };
 
 document.addEventListener('DOMContentLoaded', Game.start);
 
 Game.update = function (){
-  var d = new Date();
+  var opposite = Game.mousePosition.y - Game.cannonPosition.y;
+  var adjacent = Game.mousePosition.x - Game.cannonPosition.x;
+  Game.cannonRotation = Math.atan2(opposite, adjacent);
 };
 
 Game.draw = function () {
+  Game.clearCanvas();
   
-  
-  Game.drawImage(Game.backgroundSprite, { x: 0, y: 0 });
-  Game.drawImage(Game.ballonSprite, Game.balloonPosition);
+  Game.drawImage(Game.backgroundSprite, { x: 0, y: 0 }, 0, { x: 0, y: 0});
+  // Game.balloonOrigin = { x : Game.balloonSprite.width / 2,
+  //                        y : Game.balloonSprite.height };
+  // doesn't work as it should 
+  // This should be in a call back
+  Game.drawImage(Game.cannonBarrelSprite, Game.cannonPosition,
+        Game.cannonRotation, Game.cannonOrigin);
 };
-Game.drawImage = function (sprite, position) {
+Game.drawImage = function (sprite, position, rotation, origin) {
   Game.canvasContext.save();
   Game.canvasContext.translate(position.x, position.y);
+  Game.canvasContext.rotate(rotation);
   Game.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height,
-                                0, 0, sprite.width, sprite.height);
+                                -origin.x, -origin.y, sprite.width, sprite.height);
   Game.canvasContext.restore();
 };
 Game.clearCanvas = function () {
@@ -54,7 +57,7 @@ function handleMouseMove(evt) {
 }
 document.onmousemove = handleMouseMove;
 Game.mainLoop = function () {
-  Game.clearCanvas();
+  
   Game.update();
   Game.draw();
   window.setTimeout(Game.mainLoop, 1000 / 60);
